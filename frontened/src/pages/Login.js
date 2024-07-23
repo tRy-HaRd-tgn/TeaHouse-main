e1f8fd4bc622aeadb3dc75e8aacfa7921b60dd05
tRy-HaRd-tgn <vip.makarichev@mail.ru>
2024-07-24 00:53:40 +0300

    создал кастомные импуты и кнопки

--- frontened/src/pages/Login.js
+++ frontened/src/pages/Login.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import {Link} from "react-router-dom";
-function Login(){
-    return(
-        <div className='container'>
-            <body className='container_body'>
-                <div className='container_body_div_img'>
-                    <img className='container_body_img' src='/img/sign.svg'></img>
-                </div>
-                <form action='' className='container_form'>
-                    <input className='container_form_input' id='email' placeholder='Эл. почта' type='email' required></input>
-                    <div className='container_form_div_btn'>
-                        <button className='container_form_btn' type='submit'>Вход</button>
-                    </div>
-                </form>
-                <div className='container_body_footer'>
-                    <p className='container_footer_p'>Ещё нет аккаунта ?</p>
-                    <Link className="container_footer_a" to='/Register'>Register</Link>
-                </div>
-            </body>
+import React from "react";
+import { Link } from "react-router-dom";
+import MyLogo from "../components/logo/MyLogo";
+import MyInput from "../ui/input/MyInput";
+import MyButton from "../ui/button/MyButton";
+function Login() {
+  return (
+    <div className="container">
+      <body className="container_body">
+        <MyLogo></MyLogo>
+        <form action="" className="container_form">
+          <MyInput
+            id="email"
+            placeholder="Эл. почта"
+            type="email"
+            required
+          ></MyInput>
+          <div className="container_form_div_btn">
+            <MyButton type="submit">Вход</MyButton>
+          </div>
+        </form>
+        <div className="container_body_footer">
+          <p className="container_footer_p">Ещё нет аккаунта ?</p>
+          <Link className="container_footer_a" to="/Register">
+            Register
+          </Link>
         </div>
-    );
-} 
+      </body>
+    </div>
+  );
+}
 export default Login;
--- frontened/src/ui/button/MyButton.jsx
+++ frontened/src/ui/button/MyButton.jsx
@@ -1,9 +1,8 @@
-function MyButton(props) {
-    
-
-    return (
-        <>
-            
-        </>
-    )
+import classes from "./MyButton.module.scss";
+export default function MyButton({ children, ...props }) {
+  return (
+    <button {...props} className={classes.container_form_btn}>
+      {children}
+    </button>
+  );
 }
